9b4fec17f6af929386a4387a65d6ccc9879bea58
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-08-14 13:38:34 +0900

    add post picture

---
+++ post_picture.rb
@@ -0,0 +1,15 @@
+require 'dotenv'
+require 'koala'
+
+Dotenv.load
+
+facebook_api = Koala::Facebook::API.new(ENV['ACCESS_TOKEN'], ENV['SECRET'])
+
+file = "/tmp/#{Time.now.to_i}.jpg"
+`raspistill -o #{file}`
+puts "#{file}: saved picture"
+
+ret = facebook_api.put_picture(file, message: 'captured at #{Time.now.strftime("%Y-%m-%d %H:%M")}')
+puts "posted: #{ret}"
+
+FileUtils.remove_file(file)
