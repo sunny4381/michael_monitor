af94efc33403cb5019c7ed77c795fd05c0fffbd8
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-08-14 14:20:32 +0900

    add raspistill opts and fix timestamp string

--- post_picture.rb
+++ post_picture.rb
@@ -6,10 +6,10 @@ Dotenv.load
 facebook_api = Koala::Facebook::API.new(ENV['ACCESS_TOKEN'], ENV['SECRET'])
 
 file = "/tmp/#{Time.now.to_i}.jpg"
-`raspistill -o #{file}`
+`raspistill -o #{file} #{ENV["RASPISTILL_OPTS"]}`
 puts "#{file}: saved picture"
 
-ret = facebook_api.put_picture(file, message: 'captured at #{Time.now.strftime("%Y-%m-%d %H:%M")}')
+ret = facebook_api.put_picture(file, message: "captured at #{Time.now.strftime("%Y-%m-%d %H:%M")}")
 puts "posted: #{ret}"
 
 FileUtils.remove_file(file)
