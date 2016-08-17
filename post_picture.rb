60f7d3647d5a67635179568645d4bb366b2bef4e
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-08-17 12:48:07 +0900

    add under detection

--- post_picture.rb
+++ post_picture.rb
@@ -1,5 +1,6 @@
 require 'dotenv'
 require 'koala'
+require 'rmagick'
 
 Dotenv.load
 
@@ -9,7 +10,32 @@ file = "/tmp/#{Time.now.to_i}.jpg"
 `raspistill -o #{file} #{ENV["RASPISTILL_OPTS"]}`
 puts "#{file}: saved picture"
 
-ret = facebook_api.put_picture(file, message: "captured at #{Time.now.strftime("%Y-%m-%d %H:%M")}")
+img_list = Magick::ImageList.new(file)
+img = img_list.first
+
+sum_red = 0
+sum_green = 0
+sum_blue = 0
+img.each_pixel do |pixel, x, y|
+  sum_red += pixel.red
+  sum_green += pixel.green
+  sum_blue += pixel.blue
+end
+
+avg_red = sum_red / (img.columns * img.rows)
+avg_green = sum_green / (img.columns * img.rows)
+avg_blue = sum_blue / (img.columns * img.rows)
+
+intensity = avg_red*0.30 + avg_green*0.59 + avg_blue*0.11
+
+if intensity < 10_000
+  puts "too under"
+  FileUtils.remove_file(file)
+  exit 0
+end
+
+caption = "captured at #{Time.now.strftime("%Y-%m-%d %H:%M")}"
+ret = facebook_api.put_picture(file, caption: caption)
 puts "posted: #{ret}"
 
 FileUtils.remove_file(file)
